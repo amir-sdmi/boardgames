e9352ecbdcd0659c11d7c2a56cc1e6bc4aad88cf
Amirhossein Sedigh Daemi <89903165+amir-sdmi@users.noreply.github.com>
2024-11-18 11:36:21 +0100

    13-create-jaliz-game-logic
    WIP 02
    
    WIP03 moving player and playes in client side
    
    WIP04
    
    WIP05 finish buying actions and player details
    
    resolve issues from codeRabbit review
    
    WIP07 resolve other issues
    
    WIP08 fixing planting from hand problem

---
+++ src/app/game/components/Player.tsx
@@ -0,0 +1,44 @@
+import { useGameContext } from "@/contexts/GameContext";
+import PlayerDetails from "./PlayerDetails";
+import PlayerBuyingActions from "./PlayerBuyingActions";
+import { BuyType, PlayerType } from "@/types/gameTypes";
+import { buyAction } from "../core/actions/buyAction";
+
+export default function Player({
+  userId,
+  roomId,
+}: {
+  userId: string;
+  roomId: string;
+}) {
+  const { gameState } = useGameContext();
+  if (!gameState) {
+    return <div>Loading game state ...</div>;
+  }
+
+  const thisPlayer = gameState.players.find(
+    (player) => player.userId === userId,
+  );
+  if (!thisPlayer) {
+    return <div> Player not found in game</div>;
+  }
+
+  const handleBuy = async (
+    player: PlayerType,
+    type: BuyType,
+    price: number,
+    fieldId?: number,
+  ) => {
+    try {
+      await buyAction(roomId, player.id, type, price, fieldId);
+    } catch (error) {
+      console.error("Error buying:", error);
+    }
+  };
+  return (
+    <>
+      <PlayerDetails player={thisPlayer} roomId={roomId} />
+      <PlayerBuyingActions player={thisPlayer} handleBuy={handleBuy} />
+    </>
+  );
+}
--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,44 +1,96 @@
+import { createNewGame } from "@/app/game/core/initializeGame";
+import { cardData } from "@/app/game/utils/cardData";
+import { PLAYER_LIMITS } from "@/config/constants";
 import { db } from "@/lib/firebase";
-import { RoomType, PlayerType } from "@/types/firebaseTypes";
+import { RoomType, UserType } from "@/types/firebaseTypes";
 import {
   addDoc,
   arrayUnion,
   collection,
   doc,
+  getDoc,
   onSnapshot,
+  serverTimestamp,
   updateDoc,
+  writeBatch,
 } from "firebase/firestore";
 
 //create new room
-export const createRoom = async (
-  userId: PlayerType["id"],
-  userName: PlayerType["name"],
-) => {
-  const roomRef = collection(db, "rooms");
-  const newRoom = await addDoc(roomRef, {
-    createdBy: userId,
-    createdAt: new Date(),
-    players: [
-      {
-        id: userId,
-        name: userName,
-      },
-    ],
-    isActive: true,
-  });
-  return newRoom.id;
+export const createRoom = async (user: UserType) => {
+  try {
+    const roomRef = collection(db, "rooms");
+    const newRoom = await addDoc(roomRef, {
+      createdBy: user.id,
+      createdAt: serverTimestamp(),
+      players: [],
+      isGameStarted: false,
+    });
+    return newRoom.id;
+  } catch (error) {
+    console.error("Failed to create room:", error);
+    throw new Error("Failed to create room");
+  }
 };
 
 //Join an existing room
-export const joinRoom = async (
-  roomId: string,
-  userId: string,
-  userName: string,
-) => {
-  const roomRef = doc(db, "rooms", roomId);
-  await updateDoc(roomRef, {
-    players: arrayUnion({ id: userId, name: userName }),
-  });
+export const joinRoom = async (roomId: string, user: UserType) => {
+  try {
+    const roomRef = doc(db, "rooms", roomId);
+    const roomSnapshot = await getDoc(roomRef);
+    //Room not found
+    if (!roomSnapshot.exists()) {
+      throw new Error("Room not found");
+    }
+    //Game already started
+    const roomData = roomSnapshot.data() as RoomType;
+    if (roomData.isGameStarted) {
+      throw new Error("Game already started");
+    }
+    //full Room
+    if (roomData.players.length >= PLAYER_LIMITS.MAX) {
+      throw new Error("Room is full");
+    }
+    //duplicate users
+    if (roomData.players.some((player) => player.id === user.id)) {
+      throw new Error("You are already in the room");
+    }
+    await updateDoc(roomRef, {
+      players: arrayUnion({ id: user.id, name: user.name }),
+    });
+  } catch (error) {
+    console.error("Failed to join room:", error);
+    throw new Error("Failed to join room");
+  }
+};
+
+//Start the game
+export const startGame = async (roomId: string, players: UserType[]) => {
+  try {
+    const roomRef = doc(db, "rooms", roomId);
+    const roomSnapshot = await getDoc(roomRef);
+    if (!roomSnapshot.exists()) {
+      throw new Error("Room not found");
+    }
+    const roomData = roomSnapshot.data() as RoomType;
+    if (roomData.isGameStarted) {
+      throw new Error("Game already started");
+    }
+    if (players.length < PLAYER_LIMITS.MIN) {
+      throw new Error("Not enough players");
+    }
+
+    const gameState = createNewGame(players, cardData);
+
+    const batch = writeBatch(db);
+    batch.update(roomRef, {
+      isGameStarted: true,
+      gameState,
+    });
+    await batch.commit();
+  } catch (error) {
+    console.error("Failed to start game:", error);
+    throw new Error("Failed to start game");
+  }
 };
 
 export const listenToRoom = (
