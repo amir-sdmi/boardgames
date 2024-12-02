f24b2931fe0b5ea02706bd23189e0cf60bea1e6f
Amirhossein Sedigh Daemi <89903165+amir-sdmi@users.noreply.github.com>
2024-12-02 11:35:10 +0100

    feat: implement room joining and game functionality

--- src/app/lobby/[roomId]/page.tsx
+++ src/app/lobby/[roomId]/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import { joinRoom, listenToRoom, startGame } from "@/services/roomService";
-import { UserType } from "@/types/firebaseTypes";
+import {
+  joinRoom,
+  kickPlayer,
+  listenToRoom,
+  startGame,
+} from "@/services/roomService";
+import { RoomType } from "@/types/firebaseTypes";
 import { useUser } from "@clerk/nextjs";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { generateAddRoomLink } from "@/app/game/utils/utils";
 import { useRouter } from "next/navigation";
 import { PLAYER_LIMITS } from "@/config/constants";
+import Button from "@/app/components/ui/Button";
 
 export default function RoomPage() {
   const { roomId } = useParams<{ roomId: string }>();
   const { user } = useUser();
-  const [players, setPlayers] = useState<UserType[]>([]);
+  const [roomData, setRoomData] = useState<RoomType | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
@@ -29,21 +35,49 @@ export default function RoomPage() {
     //listen to room updates
     const unsubscribe = listenToRoom(roomId, (roomData) => {
       setLoading(false);
-      setPlayers(roomData?.players || []);
+      if (!roomData) {
+        return;
+      }
+      setRoomData(roomData);
     });
 
     return () => unsubscribe();
   }, [user, roomId]);
-
+  if (!roomData || loading) {
+    return <p>Loading room...</p>;
+  }
+  const isHost = () => roomData.createdBy === user?.id;
+  const { players } = roomData;
+  if (!players.some((player) => player.id === user?.id)) {
+    return <p>you are not soposed to be here !!!!</p>;
+  }
+  if (roomData.isGameStarted) {
+    router.push(`/game/${roomId}`);
+  }
   //TODO: make start game by being ready all players.
-  const handleStartGame = () => {
+  const handleStartGame = async () => {
+    if (!roomData) {
+      return;
+    }
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+    if (!isHost()) {
+      console.error("Only the room creator can start the game");
+      return;
+    }
     const playersCount = players.length;
     if (
       playersCount >= PLAYER_LIMITS.MIN &&
       playersCount <= PLAYER_LIMITS.MAX
     ) {
-      startGame(roomId, players);
-      router.push(`/game/${roomId}`);
+      try {
+        await startGame(roomId, players);
+        router.push(`/game/${roomId}`);
+      } catch (error) {
+        console.error("Failed to start the game:", error);
+      }
     } else {
       console.error(
         "You need at least 3 players and at most 7 to start the game",
@@ -51,7 +85,13 @@ export default function RoomPage() {
     }
   };
 
-  if (loading) return <p>Loading room...</p>;
+  const handlePlayerKick = async (playerId: string) => {
+    if (!user) {
+      console.error("User not found");
+      return;
+    }
+    await kickPlayer(roomId, playerId, user.id);
+  };
 
   return (
     <div>
@@ -63,10 +103,20 @@ export default function RoomPage() {
         <h2>Players in Room:</h2>
         <ul>
           {players.map((player) => (
-            <li key={player.id}>{player.name}</li>
+            <li key={player.id}>
+              {player.name}
+              {
+                //only host can kick other players
+                isHost() && player.id !== roomData.createdBy && (
+                  <Button onClick={() => handlePlayerKick(player.id)}>
+                    Kick
+                  </Button>
+                )
+              }
+            </li>
           ))}
         </ul>
-        <button onClick={handleStartGame}>Start Game!</button>
+        {isHost() && <Button onClick={handleStartGame}>Start Game!</Button>}
       </div>
     </div>
   );
--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -115,3 +115,26 @@ export const listenToRoom = (
     },
   );
 };
+
+export const kickPlayer = async (
+  roomId: string,
+  playerId: string,
+  requesterId: string,
+) => {
+  const roomRef = doc(db, "rooms", roomId);
+  const roomSnapshot = await getDoc(roomRef);
+  if (!roomSnapshot.exists()) {
+    throw new Error("Room not found");
+  }
+  const roomData = roomSnapshot.data() as RoomType;
+  if (roomData.createdBy !== requesterId) {
+    throw new Error("Only the host can kick players");
+  }
+  if (roomData.isGameStarted) {
+    throw new Error("Game already started");
+  }
+  const players = roomData.players.filter((player) => player.id !== playerId);
+  await updateDoc(roomRef, {
+    players,
+  });
+};
